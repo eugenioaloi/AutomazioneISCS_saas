d9440a6926c6da949eb6cf1db3ed8237e3a674e9
e.aloi <e.aloi@reply.it>
2023-12-13 16:28:54 +0100

    init porjcet BE

---
+++ node-app/automazioneSaas/src/main/java/com/Automazione_saas/AutomazioneSaasApplication.java
@@ -0,0 +1,13 @@
+package com.Automazione_saas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomazioneSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomazioneSaasApplication.class, args);
+	}
+
+}
---
+++ node-app/automazioneSaas/src/test/java/com/Automazione_saas/AutomazioneSaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Automazione_saas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomazioneSaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
